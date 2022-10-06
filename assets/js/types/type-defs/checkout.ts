44132834a7afacfe19ce4d677ad73028a3324769
Alex Florisca <alex.florisca@automattic.com>
2022-10-06 15:48:52 +0100

    Consolidate Address types (#7272)
    * Consolidate Address types
    
    * Revert "Consolidate Address types"
    
    This reverts commit cb708a3d714621717414ec17f5e1071be5ca5e96.
    
    * Rename EnteredAddress to CustomerAddress
    
    * Delete unused Address type
    
    * Thomas suggestions
    
    * Fix duplicate ShippingAddress symbol

--- assets/js/types/type-defs/checkout.ts
+++ assets/js/types/type-defs/checkout.ts
@@ -1,17 +1,10 @@
 /**
  * External dependencies
  */
-<<<<<<< HEAD
 import { ShippingAddress, BillingAddress } from '@woocommerce/settings';
 
 export interface CheckoutResponseSuccess {
 	billing_address: BillingAddress;
-=======
-import { Address } from '@woocommerce/types';
-
-export interface CheckoutResponseSuccess {
-	billing_address: Address;
->>>>>>> 7e0f79e5a (Move checkout state code into thunks and rename `CheckoutState` context to `CheckoutEvents` (#6455))
 	customer_id: number;
 	customer_note: string;
 	extensions: Record< string, unknown >;
@@ -23,11 +16,7 @@ export interface CheckoutResponseSuccess {
 		payment_status: 'success' | 'failure' | 'pending' | 'error';
 		redirect_url: string;
 	};
-<<<<<<< HEAD
 	shipping_address: ShippingAddress;
-=======
-	shipping_address: Address;
->>>>>>> 7e0f79e5a (Move checkout state code into thunks and rename `CheckoutState` context to `CheckoutEvents` (#6455))
 	status: string;
 }
 
